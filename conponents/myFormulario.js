56f92fb76276c01b648ca840b2d9cd764157eb2b
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-03-30 16:57:18 -0500

    feat: :sparkles: Se crearon las funciones de mostrar los datos desde el worker

--- conponents/myFormulario.js
+++ conponents/myFormulario.js
@@ -1,55 +1,59 @@
 /* import config from "../storage/config.js";
-
-
 datosForm(){
     config.dataFormulario();
     Object.assign(this, JSON.parse(localStorage.getItem("myFormulario")))
 
     console.log(for);
 }
-
-
  */
 export default{
 
     datosForm(){
-        
+        let suma =0;
+
         const formulario = document.querySelector('#formulario');
         formulario.addEventListener('submit', function(e){            
             e.preventDefault(); // previene la acción por defecto del formulario
 
+
             const datos = new FormData(formulario);
             const datosObj = Object.fromEntries(datos.entries());
-
+            
             const valor = parseInt(formulario.valor.value);
             const pago = formulario.pago.value;
             const selector = formulario.selector.value;
             
-            const losdatos = { 
-                valor: valor,
+            suma += valor ;
+
+            let losdatos = { 
+                valor: suma,
                 pago: pago,
-                selector:selector
+                selector:selector    
             };
-
-            console.log(datos);
-
-
+            let count = 0;
+            let id = ["saldoPositivo","agregar"]
 
             const ws = new Worker("storage/wsMyFormulario.js", {type:"module"});
-            ws.postMessage({module:"menejoData", data: this.losdatos})
 
+            ws.postMessage({module:"saldoPositivo", data: losdatos})
+            ws.postMessage({module:"agregar", data: losdatos})
 
-
-
-            console.log(valor);
-            console.log(pago);
-            console.log( selector);
             
+            ws.addEventListener("message", (e) =>{
 
-        
+            //Parseamos lo que trae el evento(mensaje)
+            let doc = new DOMParser().parseFromString(e.data, "text/html");
 
-            formulario.reset();
+            //insertamos data
+            document.querySelector("#ingresos").append(...doc.body.children);
+            document.querySelector("#saldo").prepend(...doc.body.children);
+            /*lo debo hacer con un array que me contenga cada id*/
+            })
+            
+           /*  formulario.reset(); */ /* esta lina me reseta el formulario cada que lo envio */
 
         })
+
     }
+
 }
--- storage/wsMyFormulario.js
+++ storage/wsMyFormulario.js
@@ -1,9 +1,27 @@
 let wsMyFormulario = {
 
-    menejoData(p1){
+    saldoPositivo(p1){
+        console.log(p1,"esto ya es del worker");
+        
+        let valor = p1.valor;
+        console.log(valor);
+        return `<h1 class="p-2" id="saldo">$${valor}</h1>`;
+    },
 
-        console.log(p1);
-
+    agregar(p1){
+        console.log(p1,"esto esta en agregar");
+        return `<hr>
+        <div class="d-flex bg-danger">
+            <div>
+                ${p1.pago} 
+            </div>
+            <div class="d-flex ">
+                <div>
+                    ${p1.valor}
+                </div>
+            </div>
+        </div>
+        <hr>`
     }
 
 }
