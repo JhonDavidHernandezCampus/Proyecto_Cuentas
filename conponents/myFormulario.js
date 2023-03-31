e76fcc2c1e35586fa3e56426e227b166b9f0cc4e
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-03-31 06:41:58 -0500

    feat: :sparkles:

--- conponents/myFormulario.js
+++ conponents/myFormulario.js
@@ -10,12 +10,15 @@ export default{
 
     datosForm(){
         let suma =0;
+        let ingresos=0 ;
+        let egresos=0 ;
 
         const formulario = document.querySelector('#formulario');
+
         formulario.addEventListener('submit', function(e){            
             e.preventDefault(); // previene la acción por defecto del formulario
-
-
+            
+            
             const datos = new FormData(formulario);
             const datosObj = Object.fromEntries(datos.entries());
             
@@ -25,29 +28,58 @@ export default{
             
             suma += valor ;
 
+            console.log(egresos += valor,"suma");
+
+            (selector == "suna")?ingresos += valor
+            :egresos +=valor
+
+            console.log(selector,"selector");
+            console.log(egresos,"egresos");
+            console.log(ingresos,"ingresos");
+
+
+
             let losdatos = { 
-                valor: suma,
+                valorenvio:valor,
+                valortotal: suma,
                 pago: pago,
-                selector:selector    
+                selector:selector,
+                ingresos:ingresos,
+                egresos:egresos    
             };
             let count = 0;
-            let id = ["saldoPositivo","agregar"]
+            let id = ["#saldos"]
 
             const ws = new Worker("storage/wsMyFormulario.js", {type:"module"});
 
             ws.postMessage({module:"saldoPositivo", data: losdatos})
-            ws.postMessage({module:"agregar", data: losdatos})
+            /* ws.postMessage({module:"agregar", data: losdatos}) */
 
             
             ws.addEventListener("message", (e) =>{
+                
+                const saldos = document.querySelector('#baners');
+                saldos.remove();
 
-            //Parseamos lo que trae el evento(mensaje)
-            let doc = new DOMParser().parseFromString(e.data, "text/html");
+                //Parseamos lo que trae el evento(mensaje)
+                let doc = new DOMParser().parseFromString(e.data, "text/html");
+                //insertamos data
+                document.querySelector("#saldos").append(...doc.body.children);
 
-            //insertamos data
-            document.querySelector("#ingresos").append(...doc.body.children);
-            document.querySelector("#saldo").prepend(...doc.body.children);
-            /*lo debo hacer con un array que me contenga cada id*/
+                (selector=="suma")?console.log("dentro positivo")
+                :console.log("dentro negativo");
+                document.querySelector("#saldos").append(...doc.body.children);
+
+                
+                
+                /* 
+                
+                (selector=="suma")?console.log("dentro positivo")
+                :console.log("dentro negativo");;
+
+                (id.length-1==count)?ws.terminate():count++; */
+
+                /*lo debo hacer con un array que me contenga cada id*/
             })
             
            /*  formulario.reset(); */ /* esta lina me reseta el formulario cada que lo envio */
--- storage/wsMyFormulario.js
+++ storage/wsMyFormulario.js
@@ -1,17 +1,35 @@
+
+
 let wsMyFormulario = {
 
     saldoPositivo(p1){
         console.log(p1,"esto ya es del worker");
-        
-        let valor = p1.valor;
-        console.log(valor);
-        return `<h1 class="p-2" id="saldo">$${valor}</h1>`;
+        return`
+        <div class="d-flex justify-content-center flex-column align-items-center p-5 w-100" id="baners">
+            <div id="saldo" class="saldo">
+                <h1 class="p-2">${(p1.valortotal)}</h1>
+            </div>
+            
+            <div class="d-flex align-items-center justify-content-between bg-primary w-25 m-3 rounded p-4" id="dinero">
+                <div>INGRESOS</div>
+                <div>
+                    $${p1.ingresos}
+                </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-between bg-danger w-25 rounded p-4" id="dinero">
+                <div>EGRESOS</div>
+                <div>
+                    $${p1.egresos}
+                </div>
+            </div>
+
+        </div>`
     },
 
     agregar(p1){
         console.log(p1,"esto esta en agregar");
         return `<hr>
-        <div class="d-flex bg-danger">
+        <div class="d-flex justify-content-around">
             <div>
                 ${p1.pago} 
             </div>
@@ -22,7 +40,7 @@ let wsMyFormulario = {
             </div>
         </div>
         <hr>`
-    }
+    } 
 
 }
 self. addEventListener("message",(e) => {
